baa546cc31b55e92742308765b9770378772079c
asjuan <Juan.alsu@gmail.com>
2018-07-10 22:08:46 -0700

    Initialize all components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-var p = require("./samplepresenter");
+var newPresenter = require("./samplepresenter");
+var newModel = require("./samplemodel");
+var newView = require("./sampleview");
+
+var p = newPresenter(newModel(), newView());
+p.initDefault();
